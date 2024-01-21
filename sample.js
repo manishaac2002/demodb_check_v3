a16aef6b9c521e183d527cc795d69ab09eb4dcea
manishaac2002 <manishaac2002@gmail.com>
2024-01-21 14:02:50 +0530

    feat - insert query template.

---
+++ sample.js
@@ -0,0 +1,29 @@
+
+const insertQueryTemplate = (data) => {
+  const tableName = data.tableName;
+  const columnNames = data.columnNames.join(', ');
+  const valuePlaceholders = data.valuePlaceholders.join(', ');
+
+  if (data.columnNames.length !== data.valuePlaceholders.length) {
+    console.log("Column value doesn't match the number of values, cannot insert into the table");
+    return null; // Or handle the error in a way that makes sense for your application
+  }
+
+  return (`INSERT INTO ${tableName} (${columnNames}) VALUES (${valuePlaceholders})`);
+};
+
+const data = {
+  tableName: 'employees',
+  columnNames: ['first_name', 'last_name', 'salary',],
+  valuePlaceholders: ['trdyg', 'dfghjkn', 'sdfghjkl',]
+};
+
+const insertQuery = insertQueryTemplate(data);
+
+if (insertQuery) {
+  console.log(insertQuery);
+}
+
+
+
+
